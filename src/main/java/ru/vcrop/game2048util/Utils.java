77ffcad3ae66b673d5c8b9dbefb82ab3262d04d0
vcrop <viktor.raguev@gmail.com>
2022-02-22 15:39:43 +0300

    add tests

--- src/main/java/ru/vcrop/game2048util/Utils.java
+++ src/main/java/ru/vcrop/game2048util/Utils.java
@@ -22,28 +22,31 @@ public class Utils {
         IntStream.range(0, (4 - direction.ordinal()) % 4).forEach(n -> rotateMatrix(matrix));
     }
 
+    public static void randomTile(List<List<Integer>> matrix) {
+        List<Integer> tiles = IntStream.range(0, 16).filter(n -> matrix.get(n / 4).get(n % 4).equals(0)).boxed().collect(Collectors.toList());
+        Collections.shuffle(tiles);
+        matrix.get(tiles.get(0) / 4).set(tiles.get(0) % 4, randomInt());
+    }
+
+    public static boolean full(List<List<Integer>> matrix) {
+        return matrix.stream().flatMap(List::stream).filter(i -> i != 0).count() == 16;
+    }
 
     private static void clearMatrix(List<List<Integer>> matrix) {
         for (List<Integer> row : matrix)
             for (int i = 0; i < row.size(); i++) row.set(i, 0);
     }
 
-    private static void randomTile(List<List<Integer>> matrix) {
-        List<Integer> tiles = IntStream.range(0, 16).filter(n -> !matrix.get(n / 4).get(n % 4).equals(0)).boxed().collect(Collectors.toList());
-        Collections.shuffle(tiles);
-        matrix.get(tiles.get(0) / 4).set(tiles.get(0) % 4, randomInt());
-    }
-
     private static int randomInt() {
-        return ThreadLocalRandom.current().nextInt(10) != 9 ? 2 :4;
+        return ThreadLocalRandom.current().nextInt(10) != 9 ? 2 : 4;
     }
 
     private static void leftShiftMatrix(List<List<Integer>> matrix) {
         matrix.forEach(row -> {
                     List<Integer> elems = row.stream().filter(n -> n != 0).collect(Collectors.toList());
-                    for(int i = 0; i < elems.size() - 1; i++)
+                    for (int i = 0; i < elems.size() - 1; i++)
                         if (elems.get(i).equals(elems.get(i + 1))) elems.set(i, elems.get(i) + elems.remove(i + 1));
-                        IntStream.range(0, 4).forEach(n -> row.set(n, n < elems.size() ? elems.get(n) : 0));
+                    IntStream.range(0, 4).forEach(n -> row.set(n, n < elems.size() ? elems.get(n) : 0));
                 }
         );
     }
--- src/test/java/ru/vcrop/game2048util/UtilsTest.java
+++ src/test/java/ru/vcrop/game2048util/UtilsTest.java
@@ -48,4 +48,42 @@ class UtilsTest {
         Assertions.assertEquals(matrix.get(3).get(2), 2);
         Assertions.assertNotEquals(matrix.get(1).get(2), 1);
     }
+
+    @Test
+    void initTest() {
+        int count2 = 0;
+        int count4 = 0;
+        int count_another = 0;
+        Utils.init(matrix);
+        for (List<Integer> row : matrix)
+            for (int i : row) {
+                if (i == 2) count2++;
+                else if (i == 4) count4++;
+                else if (i != 0) count_another++;
+            }
+        Assertions.assertTrue(count2 < 3);
+        Assertions.assertTrue(count4 < 2);
+        Assertions.assertEquals(count2 + count4, 2);
+        Assertions.assertEquals(0, count_another);
+    }
+
+    @Test
+    void fullTilesTest() {
+        int count2 = 0;
+        int count4 = 0;
+        int count_another = 0;
+        Utils.init(matrix);
+        for (int i = 0; i < 14; i++) Utils.randomTile(matrix);
+        for (List<Integer> row : matrix)
+            for (int i : row) {
+                if (i == 2) count2++;
+                else if (i == 4) count4++;
+                else if (i != 0) count_another++;
+            }
+        Assertions.assertTrue(count2 < 17);
+        Assertions.assertTrue(count4 < 17);
+        Assertions.assertEquals(count2 + count4, 16);
+        Assertions.assertEquals(0, count_another);
+        Assertions.assertTrue(Utils.full(matrix));
+    }
 }
